7334787948575eab666c5ff900a0b93bf8a43f8e
Micah Sterling <msterling89@gmail.com>
2021-03-04 16:01:01 -0600

    add update action with happy/sad route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,14 @@
 class Api::UsersController < ApplicationController
   def index
+    @users = User.all
     render 'index.json.jb'
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
   def create
     @user = User.new(
       first_name: params[:first_name],
@@ -13,6 +20,17 @@ class Api::UsersController < ApplicationController
     else
       render json: {message: @user.errors.full_messages}
     end
- 
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.first_name = params[:first_name] || @user.first_name
+    @user.last_name = params[:last_name] || @user.last_name
+    @user.email = params[:email] || @user.email
+    if @user.save
+      render 'show.json.jb' 
+    else
+      render json: {message: @user.errors.full_messages}
+    end
   end
 end
